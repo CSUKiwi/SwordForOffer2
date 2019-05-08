8533d01449f97f473dcf51464de95553e8a903bf
Coder_Kiwi <coder_kiwi@163.com>
2019-05-08 15:35:36 +0800

    03

--- src/Solution03.java
+++ src/Solution03.java
@@ -22,17 +22,37 @@ public class Solution03 {
         if (length == 0)
             return false;
         for (int i = 0; i < length; i++){
-            while(numbers[i] != i){
+//            while(numbers[i] != i){
+//            代码规范
+            while(i != numbers[i]){
                 if (numbers[i] == numbers[numbers[i]]){
                     duplication[0] = numbers[i];
                     return true;
                 }else{
-                    int temp = numbers[i];
-                    numbers[i] = numbers[numbers[i]];
-                    numbers[numbers[i]] = temp;
+//                    int temp = numbers[i];
+//                    numbers[i] = numbers[numbers[i]];
+//                    numbers[numbers[i]] = temp;
+//                显示运行超时，不知道原因。
+                    int temp = numbers[numbers[i]];
+                    numbers[numbers[i]] = numbers[i];
+                    numbers[i] = temp;
                 }
             }
         }
         return false;
     }
 }
+
+
+/**
+ * 解题思路:
+ * 最简单的就是用一个数组或者哈希表来存储已经遍历过的数字，但是这样需要开辟额外的空间。
+ * 如果题目要求不能开辟额外的空间，那我们可以用如下的方法：
+ * 因为数组中的数字都在0~n-1的范围内，所以，如果数组中没有重复的数，那当数组排序后，
+ * 数字i将出现在下标为i的位置。现在我们重排这个数组，从头到尾扫描每个数字，当扫描到下标为i的数字时，
+ * 首先比较这个数字(记为m)是不是等于i。如果是，则接着扫描下一个数字；如果不是，
+ * 则再拿它和m 位置上的数字进行比较，如果它们相等，就找到了一个重复的数字
+ * （该数字在下标为i和m的位置都出现了），返回true；如果它和m位置上的数字不相等，
+ * 就把第i个数字和第m个数字交换，
+ * 把m放到属于它的位置。接下来再继续循环，直到最后还没找到认为没找到重复元素，返回false。
+ * */
